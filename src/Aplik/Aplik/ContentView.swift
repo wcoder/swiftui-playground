2ac5ed8171f48b63a52e58b12d99a980a45c0933
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-04-27 01:14:44 +0300

    Add start project

---
+++ src/Aplik/Aplik/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Aplik
+//
+//  Created by Yauheni Pakala on 4/27/20.
+//  Copyright © 2020 Yauheni Pakala. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Some text!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
