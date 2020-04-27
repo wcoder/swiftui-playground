496cbdceecca8b5534b836f03050ad3ca671ca30
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-04-27 16:47:36 +0300

    Add round

--- src/Aplik/Aplik/ContentView.swift
+++ src/Aplik/Aplik/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
     @State var sliderValue = 50.0
     @State var target = Int.random(in: 1...100)
     @State var score = 0
+    @State var round = 1
     
     var body: some View {
         VStack {
@@ -48,6 +49,7 @@ struct ContentView: View {
                              dismissButton: .default(Text("Awesome!")) {
                                 self.score += self.pointsForCurrentRound()
                                 self.target = Int.random(in: 1...100)
+                                self.round += 1;
                             })
                 
             })
@@ -65,7 +67,7 @@ struct ContentView: View {
                 Text("\(score)")
                 Spacer()
                 Text("Round:")
-                Text("9999999")
+                Text("\(round)")
                 Spacer()
                 Button(action: {})
                 {
