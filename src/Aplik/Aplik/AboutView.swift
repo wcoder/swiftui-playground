c08ed31e47455c1c1086dfdfb8ec2feea0ebce82
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-04-27 17:48:06 +0300

    Add style to about page

--- src/Aplik/Aplik/AboutView.swift
+++ src/Aplik/Aplik/AboutView.swift
@@ -9,14 +9,42 @@
 import SwiftUI
 
 struct AboutView: View {
-    var body: some View {
-        VStack {
-            Text("🎯 Bullseye 🎯")
-            Text("This is Bullseye, the game where you can win points and earn fame be dragging a slider.")
-            Text("Your goal is to place the slider as close as possible to the target value. The closer you are, the more points you score.")
-            Text("Enjoy!")
+    
+    let beige = Color(red: 255.0/255.0, green: 214.0 / 255.0, blue: 179.0/255.0)
+    
+    struct AboutHeadingStyle : ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+                .foregroundColor(.black)
+                .font(Font.custom("Arial Rounded MT Bold", size: 30))
+                .padding(.top, 20)
+                .padding(.bottom, 20)
         }
-        .navigationBarTitle("About Bullseye")
+    }
+    
+    struct AboutBodyStyle : ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+                .foregroundColor(.black)
+                .font(Font.custom("Arial Rounded MT Bold", size: 16))
+                .padding(.leading, 60)
+                .padding(.trailing, 60)
+                .padding(.bottom, 20)
+        }
+    }
+    
+    var body: some View {
+        Group {
+            VStack {
+                Text("🎯 Bullseye 🎯").modifier(AboutHeadingStyle())
+                Text("This is Bullseye, the game where you can win points and earn fame be dragging a slider.").modifier(AboutBodyStyle())
+                Text("Your goal is to place the slider as close as possible to the target value. The closer you are, the more points you score.").modifier(AboutBodyStyle())
+                Text("Enjoy!").modifier(AboutBodyStyle())
+            }
+            .navigationBarTitle("About Bullseye")
+            .background(beige)
+        }
+        .background(Image("Background"))
     }
 }
 
