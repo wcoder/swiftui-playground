4f0368bc7223cac7bbb0742b3cf977c7251f14d7
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2022-06-19 16:20:38 +0300

    Create new empty project

---
+++ Scrumos/Scrumos/MeetingView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Scrumos
+//
+//  Created by Yauheni Pakala on 6/19/22.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
---
+++ Scrumos/Scrumos/ScrumosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumosApp.swift
+//  Scrumos
+//
+//  Created by Yauheni Pakala on 6/19/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
