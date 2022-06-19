67b86c503e99851cc94571580c3614564aec7417
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2022-06-19 17:09:39 +0300

    Add main page

---
+++ Scrumos/Scrumos/ScrumsView.swift
@@ -0,0 +1,27 @@
+//
+//  ScrumsView.swift
+//  Scrumos
+//
+//  Created by Yauheni Pakala on 6/19/22.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
